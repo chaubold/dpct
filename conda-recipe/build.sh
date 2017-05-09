b8e436447100b0c279dba28795223316695938d1
Stuart Berg <bergs@janelia.hhmi.org>
2017-05-09 14:28:51 -0400

    conda-recipe: On mac, use clang

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,17 +1,26 @@
 mkdir build
 cd build
 
-LINKER_FLAGS="-L${PREFIX}/lib"
-export DYLIB="dylib"
-if [ `uname` != "Darwin" ]; then
-    LINKER_FLAGS="-Wl,-rpath-link,${PREFIX}/lib ${LINKER_FLAGS}"
+if [ $(uname) == "Darwin" ]; then
+    CC=clang
+    CXX=clang++
+    CXXFLAGS="-std=c++11 -stdlib=libc++"
+    
+    export DYLIB="dylib"
+    LINKER_FLAGS="-L${PREFIX}/lib"
+else
+    CC=${PREFIX}/bin/gcc
+    CXX=${PREFIX}/bin/g++
     export DYLIB="so"
+    LINKER_FLAGS="-Wl,-rpath-link,${PREFIX}/lib -L${PREFIX}/lib"
 fi
 
+
 cmake .. \
-    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_BUILD_TYPE=Release \
     -DPYTHON_EXECUTABLE=${PYTHON} \
